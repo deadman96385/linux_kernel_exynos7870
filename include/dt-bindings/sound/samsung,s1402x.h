2b9f338f797b9371721622cd70b8f95378a12d5e
deadman96385 <seanhoyt963@gmail.com>
2026-08-31 03:05:21 -0500

    dt-bindings: sound: add Samsung S1402X mixer

---
+++ include/dt-bindings/sound/samsung,s1402x.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+#ifndef __DT_BINDINGS_SAMSUNG_S1402X_H
+#define __DT_BINDINGS_SAMSUNG_S1402X_H
+
+#define S1402X_DAI_AP0		0
+#define S1402X_DAI_CP0		1
+#define S1402X_DAI_BT		2
+#define S1402X_DAI_FM		3
+#define S1402X_DAI_AMP		4
+#define S1402X_DAI_AP1		5
+#define S1402X_DAI_CP1		6
+
+#endif /* __DT_BINDINGS_SAMSUNG_S1402X_H */
