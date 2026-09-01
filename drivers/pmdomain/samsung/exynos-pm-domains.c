3b563007361257138da3a72c09fc25134bd21956
deadman96385 <seanhoyt963@gmail.com>
2026-09-01 10:04:47 -0500

    pmdomain: samsung: add Exynos7870 domain configuration

--- drivers/pmdomain/samsung/exynos-pm-domains.c
+++ drivers/pmdomain/samsung/exynos-pm-domains.c
@@ -81,6 +81,10 @@ static const struct exynos_pm_domain_config exynos5433_cfg = {
 	.local_pwr_cfg		= 0xf,
 };
 
+static const struct exynos_pm_domain_config exynos7870_cfg = {
+	.local_pwr_cfg		= 0xf,
+};
+
 static const struct of_device_id exynos_pm_domain_of_match[] = {
 	{
 		.compatible = "samsung,exynos4210-pd",
@@ -88,6 +92,9 @@ static const struct of_device_id exynos_pm_domain_of_match[] = {
 	}, {
 		.compatible = "samsung,exynos5433-pd",
 		.data = &exynos5433_cfg,
+	}, {
+		.compatible = "samsung,exynos7870-pd",
+		.data = &exynos7870_cfg,
 	},
 	{ },
 };
